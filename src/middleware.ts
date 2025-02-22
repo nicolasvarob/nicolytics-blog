c536d35605ccd0c29457204bccc69cb4ae4fbeee
nicolasvarob <nicolas.varob@gmail.com>
2025-02-22 00:01:13 -0300

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,24 @@
 import { defineMiddleware } from "astro:middleware";
 import PocketBase from "pocketbase"
 
-export const onRequest = defineMiddleware((context,next)=> {
-    console.log('middleware !')
-    if(context.url.pathname === '/') console.log(' es el home')
+/*export const onRequest = defineMiddleware(async (context,next)=> {
+
+    const pb = new PocketBase('http://127.0.0.1:8090');
+    
+    
+    
+
+    console.log('middleware called')
+    console.log('middleware locals')
+    console.log(console.log(context.locals.pb.authStore.model))
+    console.log('isValid')
+    console.log(pb.authStore.isValid)
+   
+    if(context.url.pathname.includes('/editor')) {
+        if(!pb.authStore.isValid) return Response.redirect(new URL('/login', context.url));
+    } 
+    else if(context.url.pathname.includes('/login')) {
+        if(pb.authStore.isValid) return Response.redirect(new URL('/editor/create-post', context.url));
+    }
     return next();
-})
+})*/
