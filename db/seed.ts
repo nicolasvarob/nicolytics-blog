2901dca178bfca12b232bc82776eb24f21d510de
nicolasvarob <nicolas.varob@gmail.com>
2025-03-05 00:47:17 -0300

    hide data

---
+++ db/seed.ts
@@ -0,0 +1,11 @@
+import { v4 as UUID } from "uuid"
+import bcrypt from "bcryptjs"
+
+
+export default async function seed(){
+
+    const roles =[
+        {id:'admin',name:'Administrador'},
+        {id:'user',name:'Usuario escritor'},
+    ]
+}
