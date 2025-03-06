1af0021c599c9d422a3164185e2a558c1d596f04
nicolasvarob <nicolas.varob@gmail.com>
2025-03-06 01:58:52 -0300

    text editor updates

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,28 @@
 import { defineMiddleware } from "astro:middleware"
 import { getSession } from "auth-astro/server";
 
-const nonAuthenticatedRoutes = ['/login']
 export const onRequest = defineMiddleware(
     async ({ url, locals, redirect, request }, next) => {
       
       const session = await getSession(request)
-      console.log(session)
+      
       const isLoggedIn = !!session;
 
       locals.isLoggedIn = isLoggedIn;
       locals.user = {
-        name: session?.user?.name,
-        email: session?.user?.email,
+        name: session?.user.name ?? '',
+        email: session?.user.email ?? '',
       }
 
-      if(!isLoggedIn && url.pathname.startsWith('/editor')){
+
+      if(
+        !isLoggedIn 
+        && url.pathname.startsWith('/editor')){
         return redirect('/login')
-      } else if (isLoggedIn && url.pathname == "/login" ){
+      } 
+      else if (
+        isLoggedIn 
+        && (url.pathname == "/login"||url.pathname == "/editor" ) ){
         return redirect('/editor/list-posts')
       }
   
